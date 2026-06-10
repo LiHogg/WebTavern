da0a7995146cbe10c5132632cb5e256dab052eb0
LiHogg <123923344+LiHogg@users.noreply.github.com>
2026-06-10 12:09:02 +0300

    Enable booking flow adjustments

--- backend/apps/bookings/apps.py
+++ backend/apps/bookings/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
 
+
 class BookingsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.bookings"
     verbose_name = "Bookings"
+
+    def ready(self):
+        from .flow_patch import apply_booking_flow_patch
+        apply_booking_flow_patch()
